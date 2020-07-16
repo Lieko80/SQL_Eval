a042b786984923d7a589be16b901c41d77d03da1
80010-80-02 <80010-80-02@exchange.ad.afpanet>
2020-07-16 09:16:07 +0200

    derniere version

--- eval_exo1.sql
+++ eval_exo1.sql
@@ -1,36 +1,32 @@
 DROP DATABASE IF EXISTS TB_exo1;
 CREATE DATABASE TB_exo1;
 
-DROP TABLE IF EXISTS est_compose;
 CREATE TABLE est_compse (
-    com_num INT, 
-    pro_num INT,
+    com_num INT NOT NULL AUTO_INCREMENT, 
+    pro_num INT NOT NULL AUTO_INCREMENT,
     est_qte INT,
     PRIMARY KEY (com_num,pro_num)
+    FOREIGN KEY (pro_num) REFERENCES Produit(pro_num)
+    FOREIGN KEY (com_num) REFERENCES Commande(com_num)
 );
 
-DROP TABLE IF EXISTS Produit;
 CREATE TABLE Pdroduit(
-    pro_num INT,
+    pro_num INT NOT NULL AUTO_INCREMENT,
     pro_libelle VARCHAR(50),
     pro_description VARCHAR(50),
     PRIMARY KEY (pro_num),
-    FOREIGN KEY (pro_num) REFERENCES est_compose(pro_num)
 );
 
-DROP TABLE IF EXISTS Commande;
 CREATE TABLE Commande(
-    com_num INT,
+    com_num INT NOT NULL AUTO_INCREMENT,
     cli_num INT,
     com_date DATETIME,
     com_obs VARCHAR (50),
     PRIMARY KEY (com_num),
-    FOREIGN KEY (com_num) REFERENCES est_compose(com_num)
 );
 
-DROP TABLE IF EXISTS Client;
 CREATE TABLE Client(
-    cli_num INT,
+    cli_num INT NOT NULL AUTO_INCREMENT,
     cli_nom VARCHAR(50),
     cli_adresse VARCHAR(50),
     cli_tel VARCHAR(30),
